69232c364a0bb2521be127c6a99f2305cf016dc9
ljuglaret <ljuglaret@gmail.com>
2021-12-24 03:22:16 +0100

    Updates

---
+++ precache-manifest.57155d82e05b4aed1c896af108c882cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85fdd8d5ec54eaa933d9",
+    "url": "/hnyapp/static/css/main.3cdb6f9d.chunk.css"
+  },
+  {
+    "revision": "85fdd8d5ec54eaa933d9",
+    "url": "/hnyapp/static/js/main.40e4a9a1.chunk.js"
+  },
+  {
+    "revision": "65132be3a1511f7f1c69",
+    "url": "/hnyapp/static/js/runtime~main.22e71f7f.js"
+  },
+  {
+    "revision": "f59eb9852c349daf0937",
+    "url": "/hnyapp/static/js/2.c362f49e.chunk.js"
+  },
+  {
+    "revision": "312ff34b36bcb6695a4b",
+    "url": "/hnyapp/static/js/3.d3c67870.chunk.js"
+  },
+  {
+    "revision": "c75eb08d749c9fa887174d4d870f682f",
+    "url": "/hnyapp/index.html"
+  }
+];
