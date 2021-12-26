3c2e71760ee04d5a8bb2d96ae973f42c3cf016e6
ljuglaret <ljuglaret@gmail.com>
2021-12-26 23:30:12 +0100

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -4,100 +4,65 @@ import './App.css';
 import * as dataL from '../src/assets/languages.json';
 import * as dataC from '../src/assets/countries.json';
 import * as dataW from '../src/assets/Wishes.json';
+import { Zlib } from 'zlib';
 const fs = require('fs');
 const divDOWN = document.createElement("div");
 divDOWN.id="message";
 document.body.appendChild(divDOWN)
-let myObj = JSON.stringify(dataC);
-myObj  = myObj.replace(/[\u007F-\uFFFF]/g, function(chr) {
-  return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).substring(-4)
-})
-let countriesList : string[] = []
 
-let countrieAndName: { value: string }[] = []
-let countrieAndLanguage: { language: LanguageT }[] = []
-let countrieAndWishes: { language: LanguageT,wishe: LanguageT}[] = []
+let countriesName : {value:string}[] = []
 
-let myObjL = JSON.stringify(dataL);
-myObjL  = myObjL.replace(/[\u007F-\uFFFF]/g, function(chr) {
-  return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).substring(-4)
-})
-let myObjW = JSON.stringify(dataW);
-myObjW  = myObjW.replace(/[\u007F-\uFFFF]/g, function(chr) {
-  return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).substring(-4)
-})
-type LanguageT = string
-
-interface MyObj {
-  Language: LanguageT
-  Wishe: string
-}
-myObjW = myObjW.slice(11,myObjW.length - 1)
-let obj :MyObj[]  = JSON.parse(myObjW)
-
+let countriesLanguage: { language:string}[] = []
+let countriesWishe : { language: string,wishe: string}[] = []
 
 const App: React.FunctionComponent = () => {
  
-  function searchWishesForALanguage (language : LanguageT) :string {
+  function searchWishesForALanguage (languageW : string) :string {
     let res: string = ""
-    for (let i = 0 ; i < obj.length ; i++){
-      if(language.toLocaleLowerCase().includes(obj[i].Language.toLocaleLowerCase())){
-        res = obj[i].Wishe;
+    for (let i = 0 ; i < dataW.list.length ; i++){
+      if ((dataW.list[i].Language).toLowerCase() == languageW.toLowerCase()){
+        res = dataW.list[i].Wishe
       }
     }
     return res
   }
-  function countries(): string[] {
-    for(let i = 0 ; i < myObj.split("},").length ; i++){
-      let str : string = (myObj.split("},")[i].split(",")[0]).split(":")[2]
-      str = str.replaceAll('\"' , '')
-      if (!str.includes("name")){        
-        countriesList.push(str);
-        countrieAndName.push(  { "value": str });
-      }
-  }
-  return countriesList
+  var keysC : string[]= Object.keys(dataC.countries)
+  function countries(): void {
+    for (let i = 0 ; i < keysC.length ; i++){
+      //@ts-ignore
+      countriesName.push({"value":dataC.countries[keysC[i]].name});
+    } 
 }
 
   const [selectedOption, setSelectedOption] = useState<String>();
   const selectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
-    countrieAndLanguage=[]
-    countrieAndWishes = []
-
-  for(let i = 0 ; i < myObj.split("},").length ; i++){
-    let str : String = myObj.split("},")[i]
-    if(str.includes('"name":"' +value+'"')){
-      let allLanguages  = str.split(":")[8].split(",")
-      for (let k = 0 ; k < allLanguages.length ; k++){
-        let s : string = allLanguages[k];
-        s  = s.substring(1,s.length-1);
-        s = s.replaceAll('\"','');
-        s = "\""+s+"\"";
-        
-
-        for(let i = 0 ; i < myObjL.split("},").length ; i++){
-          let str : String = myObjL.split("},")[i]
-          if(str.includes(s)){
-            var l = str.split(":")[2].split(",")[0]
-            countrieAndLanguage.push(  { "language":l });
-            countrieAndWishes.push({"language": l,wishe:searchWishesForALanguage(l)})
-          }
-        }
-      }
-
+    countriesLanguage=[]
+    countriesWishe = []
+    for (let i = 0 ; i < keysC.length ; i++){
+      //@ts-ignore
+      if (value ==  dataC.countries[keysC[i]].name){
+        //@ts-ignore
+        var allL = dataC.countries[keysC[i]].languages;
+        //@ts-ignore
+        for (let j = 0 ; j < allL.length ; j++){
+          //@ts-ignore
+          countriesLanguage.push( {"language" : dataL.languages[allL[j]].name } )
+          countriesWishe.push({"language": allL[j],wishe:searchWishesForALanguage(allL[j])});
+       }
     }
-  } 
+  }
     setSelectedOption(value);
   };
   countries();
+
   return (
     <div className='App' >
       <select defaultValue={'DEFAULT'} className='App-select' onChange={selectChange} >
         <option selected disabled>
           Choose one
         </option>
-        {countrieAndName.map((option) => (
+        {countriesName.map((option) => (
             <option value={option.value}>{option.value}</option>
   
         ))}          
@@ -107,7 +72,7 @@ const App: React.FunctionComponent = () => {
         <table>
           <thead></thead>
           <tbody>
-        {countrieAndLanguage.map((option) => (
+        {countriesLanguage.map((option) => (
             <tr>
               <td>{option.language}</td>
               <td onClick={() => sendMail(searchWishesForALanguage(option.language))}>{searchWishesForALanguage(option.language) }</td>
@@ -120,14 +85,11 @@ const App: React.FunctionComponent = () => {
     </div>
   );
 };
-//onClick={() => this.fetchData("dfd")}
-//sendMail('e')
 
 function sendMail(message : string){
   let mail = document.createElement("a");
   mail.href = "mailto:?subject=2022&body="+message+" 2022 !"+"🥳";
   mail.click();
-  //alert("mail : " + message)
 }
 
 export const messageChoisi:string = "message-A-Afficher"
