d9fc633f73b8c2154efa03bc404d8d91ace0dcd9
ljuglaret <ljuglaret@gmail.com>
2021-12-26 23:31:01 +0100

    Updates

--- precache-manifest.34d43ab87848fb66ecdc244fb9429497.js
+++ precache-manifest.34d43ab87848fb66ecdc244fb9429497.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "6c0e9e40ffcd59ad84bb",
+    "revision": "71013155118419efc58e",
     "url": "/hnyapp/static/css/main.6d870537.chunk.css"
   },
   {
-    "revision": "6c0e9e40ffcd59ad84bb",
-    "url": "/hnyapp/static/js/main.75ebf225.chunk.js"
+    "revision": "71013155118419efc58e",
+    "url": "/hnyapp/static/js/main.d6a0da55.chunk.js"
   },
   {
     "revision": "65132be3a1511f7f1c69",
     "url": "/hnyapp/static/js/runtime~main.22e71f7f.js"
   },
   {
-    "revision": "f59eb9852c349daf0937",
-    "url": "/hnyapp/static/js/2.c362f49e.chunk.js"
+    "revision": "97e14e944971549a3820",
+    "url": "/hnyapp/static/js/2.f25f13a8.chunk.js"
   },
   {
     "revision": "312ff34b36bcb6695a4b",
     "url": "/hnyapp/static/js/3.d3c67870.chunk.js"
   },
   {
-    "revision": "abadc8953667a77f286f17ae6ecacbbd",
+    "revision": "ad970faa4bc316818fa694594d4cf484",
     "url": "/hnyapp/index.html"
   }
 ];
